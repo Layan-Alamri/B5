bb5758001f41123b64bc0f47d657f52cb163d7c8
Hassan Algoz <hassanalgoz@gmail.com>
2026-01-19 09:23:27 +0300

    W6: remove old D3 since Agents (previously D4) gets there eventually. Otherwise this is duplication without value

--- W6/D3/tools/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Tools package for database operations."""
-
-from .list_tables import list_tables
-from .describe_table import describe_table
-from .execute_query import execute_query
-
-__all__ = ["list_tables", "describe_table", "execute_query"]
